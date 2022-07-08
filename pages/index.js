ad0a9dba36021211beb043a6169f03c1d1710f8c
lucasjolibois54 <lucasjolibois@icloud.com>
2022-07-08 23:41:14 +0200

    setup innit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Landing from '../components/Landing'
+
+export default function Home() {
+  return (
+  <div>
+    <Landing/>
+  </div>
+  )
+}
