b4a21f0871fcc16797c239008b1056662bfab7ef
lucasjolibois54 <lucasjolibois@icloud.com>
2022-07-08 23:45:00 +0200

    Update Landing.js

--- components/Landing.js
+++ components/Landing.js
@@ -38,7 +38,7 @@ export default function Landing() {
         <div class="w-full pb-6 space-y-6 sm:max-w-md lg:max-w-lg md:space-y-4 lg:space-y-8 xl:space-y-9 sm:pr-5 lg:pr-0 md:pb-0">
           <h1 class="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-4xl lg:text-5xl xl:text-6xl">
             <span class="block xl:inline">Useful Tools to</span>
-            <span class="block text-indigo-600 xl:inline">Help You Build Faster.</span>
+            <span class="block text-indigo-600 xl:inline"> Help You Build Faster.</span>
           </h1>
           <p class="mx-auto text-base text-gray-500 sm:max-w-md lg:text-xl md:max-w-3xl">It's never been easier to build beautiful websites that convey your message and tell your story.</p>
           <div class="relative flex flex-col sm:flex-row sm:space-x-4">
